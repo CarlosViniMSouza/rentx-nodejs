46ed5d88fd3721cfdb8479df2df634c0a8f6d36f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-16 10:25:26 -0300

    infra folder add

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add .
-git commit -m "rentx nodejs inserted"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/rentx-nodejs.git
+git commit -m "infra folder add"
 git push -u origin main
 git status
